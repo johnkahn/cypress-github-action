7848fb854ea252bc5c12afc94e35f247d77c1452
John Kahn <hello@jkahn.me>
2020-10-30 00:20:14 -0500

    Test failure

---
+++ examples/basic/cypress/integration/another.spec.js
@@ -0,0 +1,9 @@
+it('works', () => {
+  expect(42).to.equal(21 + 21)
+  cy.visit('https://example.cypress.io')
+})
+
+it('doesnt work', () => {
+  expect(43).to.equal(21 + 21)
+  cy.visit('https://example.cypress.io')
+})
