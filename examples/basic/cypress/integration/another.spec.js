4e212381b36a25da3264655249177db04152f937
John Kahn <hello@jkahn.me>
2020-10-30 00:58:50 -0500

    Remove another spec

--- examples/basic/cypress/integration/another.spec.js
+++
@@ -1,9 +0,0 @@
-it('doesnt work again', () => {
-  expect(44).to.equal(21 + 21)
-  cy.visit('https://example.cypress.io')
-})
-
-it('doesnt work', () => {
-  expect(43).to.equal(21 + 21)
-  cy.visit('https://example.cypress.io')
-})
